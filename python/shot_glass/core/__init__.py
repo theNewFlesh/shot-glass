b5d3b977988e0b1de7632a9e25aa47d5ec07d7d4
Alex Braun <alexander.g.braun@gmail.com>
2023-04-22 18:01:05 +0000

    added monad class

---
+++ python/shot_glass/core/__init__.py
@@ -0,0 +1,1 @@
+import shot_glass.core.monad  # noqa F401
---
+++ python/shot_glass/core/monad.py
@@ -0,0 +1,77 @@
+from typing import Callable, Generic, TypeVar  # noqa: F401
+A = TypeVar('A')
+B = TypeVar('B')
+# ------------------------------------------------------------------------------
+
+
+def wrap(data):
+    # type: (A) -> Monad[A]
+    return Monad(data)
+
+
+def unwrap(monad):
+    # type: (Monad[A]) -> A
+    return monad._data
+
+
+def fmap(monad, func):
+    # type: (Monad[A], Callable[[A], B]) -> Monad[B]
+    return wrap(func(unwrap(monad)))
+
+
+def applicative(monad, monad_func):
+    # type: (Monad[A], Monad[Callable[[A], B]]) -> Monad
+    func = unwrap(monad_func)
+    value = unwrap(monad)
+    return wrap(func(value))
+
+
+def bind(monad, func):
+    # type: (Monad[A], Callable[[A], Monad[B]]) -> Monad[B]
+    return func(unwrap(monad))
+
+
+def right(monad_a, monad_b):
+    # type: (Monad[A], Monad[B]) -> Monad[B]
+    return monad_b
+
+
+def fail(error):
+    # type (str) -> Monad[str]
+    return wrap(error)
+# ------------------------------------------------------------------------------
+
+
+class Monad(Generic[A]):
+    def __init__(self, data):
+        # type: (A) -> None
+        self._data = data
+
+    @staticmethod
+    def wrap(data):
+        # type: (A) -> Monad[A]
+        return wrap(data)
+
+    def unwrap(self):
+        # type: () -> A
+        return unwrap(self)
+
+    def fmap(self, func):
+        # type: (Callable[[A], B]) -> Monad[B]
+        return fmap(self, func)
+
+    def applicative(self, monad_func):
+        # type: (Monad[Callable[[A], B]]) -> Monad
+        return applicative(self, monad_func)
+
+    def bind(self, func):
+        # type: (Callable[[A], Monad[B]]) -> Monad[B]
+        return bind(self, func)
+
+    def right(self, monad_b):
+        # type: (Monad[B]) -> Monad[B]
+        return right(self, monad_b)
+
+    def fail(self, error):
+        # type (str) -> Monad[str]
+        return fail(error)
